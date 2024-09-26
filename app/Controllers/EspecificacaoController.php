ff8f32b36e656b2e8a6debfb2c6355fe0b1e7744
VagnerRGSB <vagnerrgsb@gmail.com>
2024-09-26 18:27:02 -0300

    Atualiza especificacao

--- app/Controllers/EspecificacaoController.php
+++ app/Controllers/EspecificacaoController.php
@@ -3,13 +3,27 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\CategoriaPecaModel;
+use App\Models\EspecificacaoModel;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class EspecificacaoController extends BaseController
 {
-    public function listar()
+    private $especificacao;
+    private $categoria_peca;
+
+    public function __construct()
     {
-        echo view("layouts/header");
+        $this->especificacao = new EspecificacaoModel();
+        $this->categoria_peca = new CategoriaPecaModel();
+
+    }
+    public function listar()
+    { 
+        echo view("layouts/header", [
+            "itens" => $this->especificacao->orderBy("nome")->paginate(10),
+            "pager" => $this->especificacao->pager
+        ]);
         echo view("layouts/menuMaster");
         echo view("especificacao/listar");
         echo view("layouts/footer");
